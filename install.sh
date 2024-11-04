79e7b6508ed1ab7e6cdfdcdeb19600eb28eb1bd4
wael ben <1120w@protonmail.com>
2024-11-04 17:14:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,14 +5,12 @@ caddyVersion=2.1.1
 echo "Install xcaddy (for building Caddy with custom modules)"
 curl -s -O -L https://github.com/caddyserver/xcaddy/releases/download/v0.2.0/xcaddy_0.2.0_linux_amd64.tar.gz
 tar xf xcaddy_0.2.0_linux_amd64.tar.gz
-sudo mv xcaddy /usr/local/bin/
 rm xcaddy_0.2.0_linux_amd64.tar.gz
 
 echo "Download and build Caddy with Cloudflare DNS provider"
 xcaddy build v${caddyVersion} --with github.com/caddy-dns/cloudflare
 
 echo "Move Caddy binary to /usr/local/bin/"
-sudo mv caddy /usr/local/bin/
 
 echo "Enable Caddy to bind low ports"
 sudo setcap 'cap_net_bind_service=+ep' /usr/local/bin/caddy
